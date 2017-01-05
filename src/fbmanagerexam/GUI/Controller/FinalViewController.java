01c1e92604c2a40b96136f55dd66617e744831b5
dani5636 <dani5636@easv365.dk>
2017-01-05 12:29:36 +0100

    FinalView should now look the same for everyone :smile:
    woop

--- src/fbmanagerexam/GUI/Controller/FinalViewController.java
+++ src/fbmanagerexam/GUI/Controller/FinalViewController.java
@@ -7,7 +7,12 @@ package fbmanagerexam.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
 
 /**
  * FXML Controller class
@@ -16,12 +21,47 @@ import javafx.fxml.Initializable;
  */
 public class FinalViewController implements Initializable {
 
+    @FXML
+    private Label lblQ1Team1;
+    @FXML
+    private Label lblQ1Team2;
+    @FXML
+    private Label lblQ2Team1;
+    @FXML
+    private Label lblQ2Team2;
+    @FXML
+    private Label lblQ3Team1;
+    @FXML
+    private Label lblQ3Team2;
+    @FXML
+    private Label lblQ4Team1;
+    @FXML
+    private Label lblQ4Team2;
+    @FXML
+    private Label lblHeadline;
+    @FXML
+    private ImageView imgBracket;
+    @FXML
+    private Label lblS1Team1;
+    @FXML
+    private Label lblS2Team1;
+    @FXML
+    private Label lblS2Team2;
+    @FXML
+    private Label lblQ1Team11211;
+    @FXML
+    private Label lblFinal;
+    @FXML
+    private Label lblQ1Team1131;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        imgBracket.setImage(new Image("file:Resources/Pictures/Bracket.png"));
+        lblHeadline.setFont(Font.loadFont("file:Resources/Fonts/Enchanted Land.otf", 150));
+    }
+
 }

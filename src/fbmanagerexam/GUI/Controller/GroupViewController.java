4d23e028d543ae0fb1e7d0129387b11f18f4e268
Gudlauggu <gudl0025@easv365.dk>
2017-01-10 09:39:33 +0100

    some things in group view

--- src/fbmanagerexam/GUI/Controller/GroupViewController.java
+++ src/fbmanagerexam/GUI/Controller/GroupViewController.java
@@ -5,6 +5,10 @@
  */
 package fbmanagerexam.GUI.Controller;
 
+import fbmanagerexam.BE.Match;
+import fbmanagerexam.BE.Team;
+import fbmanagerexam.GUI.Model.MatchModel;
+import fbmanagerexam.GUI.Model.TeamModel;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -13,28 +17,52 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
 
 /**
  * FXML Controller class
  *
  * @author Mecaa
  */
-public class GroupViewController extends ParentController implements Initializable {
-    
-    
+public class GroupViewController extends ParentController implements Initializable
+{
+
     @FXML
     private ChoiceBox<String> CBgroups;
+    @FXML
+    private TableColumn<Team, String> clmTeamID;
+    @FXML
+    private TableColumn<Team, String> clmTeamName;
+    @FXML
+    private TableColumn<Team, String> clmRank;
+    @FXML
+    private TableColumn<Match, String> clmMatchID;
+    @FXML
+    private TableColumn<Match, String> clmMatchHTeam;
+    @FXML
+    private TableColumn<Match, String> clmMatchATeam;
+    @FXML
+    private TableView<Team> tblTeam;
+    @FXML
+    private TableView<Match> tblMatch;
+
+    private TeamModel teamModel = TeamModel.getTeamModel();
+    private MatchModel matchModel = MatchModel.getMatchModel();
+
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+      {
         // TODO
         GroupChoicer();
-    }  
+        
+      }
 
     @FXML
     private void closeWindow(ActionEvent event)
@@ -43,18 +71,33 @@ public class GroupViewController extends ParentController implements Initializab
         stage.close();
       }
 
-    
-    
-    
-    
-    private void GroupChoicer() {
-        
-    ObservableList<String> groups = FXCollections.observableArrayList("Group A", "Group B", "Group C", "Group D");
-    CBgroups.setItems(groups);
-    CBgroups.setValue("Group A");
-    
-    }
-    
-     
-        
-    }
+    private void GroupChoicer()
+      {
+
+        ObservableList<String> groups = FXCollections.observableArrayList("Group A", "Group B", "Group C", "Group D");
+        CBgroups.setItems(groups);
+        CBgroups.setValue("Group A");
+        updateFields();
+
+      }
+
+    private void updateFields()
+      {
+        tblTeam.setItems(teamModel.getTeams());
+        clmTeamID.setCellValueFactory(
+                new PropertyValueFactory("id"));
+        clmRank.setCellValueFactory(
+                new PropertyValueFactory("point"));
+        clmTeamName.setCellValueFactory(
+                new PropertyValueFactory("name"));
+
+        tblMatch.setItems(matchModel.getMatches());
+        clmMatchHTeam.setCellValueFactory(
+                new PropertyValueFactory("homeTeamName"));
+        clmMatchATeam.setCellValueFactory(
+                new PropertyValueFactory("awayTeamName"));
+        clmMatchID.setCellValueFactory(
+                new PropertyValueFactory("matchId"));
+      }
+
+}
